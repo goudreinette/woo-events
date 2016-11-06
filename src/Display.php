35fed5463453303f28247eb83224da6f4a31e995
Rein van der Woerd <rwoerd2033@student.landstede.nl>
2016-11-06 12:12:08 +0100

    busy with add to cart redirect

--- src/Display.php
+++ src/Display.php
@@ -7,6 +7,8 @@ class Display
         add_action('woocommerce_before_shop_loop_item_title', [$this, 'shopLoop']);
         add_action('woocommerce_single_product_summary', [$this, 'singleProduct']);
         add_action('woocommerce_order_item_meta_start', [$this, 'emails'], 10, 4);
+        add_filter('woocommerce_add_to_cart_redirect', [$this, 'cartUrl'], 10, 4);
+
     }
 
     function shopLoop()
@@ -58,6 +60,18 @@ class Display
                 <span class='start'>{$startDate}</span> - 
                 <span class='end'>{$endDate}</span>
             </h3>";
+        }
+    }
+
+    function cartUrl($original)
+    {
+        global $post;
+        $meta = Meta::get($post->ID);
+
+        if ($meta && $meta['external-link']) {
+            return $meta['external-link'];
+        } else {
+            return $original;
         }
     }
 }
