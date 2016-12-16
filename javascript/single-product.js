c80f1b0252035ec2c57366f3aa183c61c9ac3244
reinvdwoerd <reinvanderwoerd@me.com>
2016-12-16 17:25:41 +0100

    refactor finished

--- javascript/single-product.js
+++ javascript/single-product.js
@@ -3,7 +3,7 @@ jQuery(function ($) {
     if (assigns['hideButton']) {
         $('form.cart button').remove()
     } else {
-        $('form.cart button').text(assigns['cart-button-text'])
+        $('form.cart button').text(assigns['cartButtonText'])
     }
 
     $('.quantity, .add_to_wishlist, .price-container').remove()
